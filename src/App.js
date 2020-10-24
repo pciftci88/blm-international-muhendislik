4332bd9f2d37a804accc3763b1da5607b78b13f0
Philipp Ciftci <pciftci@MacBook-Pro.fritz.box>
2020-10-24 18:34:56 +0200

    Fix routing for gh pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Homepage from './pages/homepage/Homepage';
 import ProductOverview from './pages/product-overview/ProductOverview';
 import Impressum from './pages/impressum/Impressum';
-import Contact from './pages/contact/Contact';
 import About from './pages/about/About';
 import BrandsOverview from './pages/brands-overview/BrandsOverview';
 import MobileToolbar from './components/mobile-toolbar/MobileToolbar';
@@ -29,22 +28,23 @@ class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        {this.state.isMobile ? (
-          <React.Fragment>
-            <MobileToolbar />
-            <ContactBlock />
-          </React.Fragment>
-        ) : (
-            <Header />
-          )}
-        <Switch>
-          <Route exact path='/' component={Homepage} />
-          <Route exact path='/produkte' component={ProductOverview} />
-          <Route exact path='/impressum' component={Impressum} />
-          <Route exact path='/kontakt' component={Contact} />
-          <Route exact path='/ueber-uns' component={About} />
-          <Route exact path='/markenuebersicht' component={BrandsOverview} />
-        </Switch>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          {this.state.isMobile ? (
+            <React.Fragment>
+              <MobileToolbar />
+              <ContactBlock />
+            </React.Fragment>
+          ) : (
+              <Header />
+            )}
+          <Switch>
+            <Route exact path='/' component={Homepage} />
+            <Route exact path='/produkte' component={ProductOverview} />
+            <Route exact path='/impressum' component={Impressum} />
+            <Route exact path='/ueber-uns' component={About} />
+            <Route exact path='/markenuebersicht' component={BrandsOverview} />
+          </Switch>
+        </BrowserRouter>
         <Footer />
       </React.Fragment>
     );
